79647eec5541f42dcfb46e9fbdf7c545cb398072
Ana Ganfornina <anies.llamani@gmail.com>
2025-06-12 17:10:23 +0200

    Initial commit

---
+++ AGMarvelApp/AGMarvelAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AGMarvelAppApp.swift
+//  AGMarvelApp
+//
+//  Created by Ana on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct AGMarvelAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
